a50075af7a775ee1aa26dbc7e32f2614cc3915e9
Chikaysci <kjabellar@gmail.com>
2026-06-24 13:58:44 +0800

    Remove [isAdmin] debug console.logs from auth callbacks

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -45,11 +45,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           id: member.id,
           is_admin: member.is_admin,
         });
-        console.log("[isAdmin] credentials login", {
-          memberId: member.id,
-          email: member.email,
-          isAdmin,
-        });
 
         return {
           id: String(member.id),
@@ -84,12 +79,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         token.isAdmin =
           !Number.isNaN(mid) &&
           (fromUser || (await memberIdIsPlatformAdmin(mid)));
-        console.log("[isAdmin] jwt callback", {
-          memberId: user.id,
-          email: user.email,
-          fromUser,
-          isAdmin: token.isAdmin,
-        });
       }
       return token;
     },
@@ -98,11 +87,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         session.user.id = token.id as string;
         const u = session.user as typeof session.user & { isAdmin?: boolean };
         u.isAdmin = Boolean(token.isAdmin);
-        console.log("[isAdmin] session callback", {
-          memberId: token.id,
-          email: session.user.email,
-          isAdmin: u.isAdmin,
-        });
       }
       return session;
     },
